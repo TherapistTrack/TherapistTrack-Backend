3e06ad248125958417ed8406f8ca563775c044ff
DanielRasho <ray22933@uvg.edu.gt>
2024-07-15 12:37:12 -0600

    feat: create dummy test

--- tests/routes/authRoutes.test.js
+++ tests/routes/authRoutes.test.js
@@ -1,7 +1,29 @@
 const axios = require('axios')
+const { BASE_URL } = require('../jest.setup')
 
 function sum(a, b) {
   return a + b
 }
 
-describe('Authorization API paths', () => {})
+test('dummy test', async () => {
+  const url = BASE_URL + 'login/'
+  const data = {
+    username: 'dummy',
+    password: '1234'
+  }
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  try {
+    const response = await axios.post(url, data, config)
+
+    console.log(response.data)
+    expect(response.status).toBe(200)
+  } catch (error) {
+    console.error('Error making POST request:', error)
+    throw error
+  }
+})
