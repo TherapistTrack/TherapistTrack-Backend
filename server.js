d26a5d06e391912c273ba92b1decb3fedab7663c
DanielRasho <ray22933@uvg.edu.gt>
2024-07-24 10:21:23 -0600

    feat: adding delay for starting backend service
    The delay is controlled by a environment variable on .env file

--- server.js
+++ server.js
@@ -3,31 +3,39 @@ const express = require('express')
 const cors = require('cors')
 const setupSwagger = require('./docs/swaggerConfig')
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+async function main() {
+  await sleep(process.env.DELAY_START)
+  const app = express()
+  app.use(express.json())
+  app.use(cors())
 
-// Swagger setup
-setupSwagger(app)
+  // Swagger setup
+  setupSwagger(app)
 
-// Import Routes
-const userRoutes = require('./routes/userRoutes')
-const fileRoutes = require('./routes/fileRoutes')
-const authRoutes = require('./routes/authRoutes')
+  // Import Routes
+  const userRoutes = require('./routes/userRoutes')
+  const fileRoutes = require('./routes/fileRoutes')
+  const authRoutes = require('./routes/authRoutes')
 
-// Use Routes
-app.use('/api/users', userRoutes)
-app.use('/api/files', fileRoutes)
-app.use('/api/auth', authRoutes)
+  // Use Routes
+  app.use('/api/users', userRoutes)
+  app.use('/api/files', fileRoutes)
+  app.use('/api/auth', authRoutes)
 
-// Error Handling Middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
-})
+  // Error Handling Middleware
+  app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+  })
 
-// Start the Server
-const PORT = process.env.API_PORT || 3000
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  // Start the Server
+  const PORT = process.env.API_PORT || 3000
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
+
+main()
