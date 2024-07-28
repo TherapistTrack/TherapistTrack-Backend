e15e04990a8ba5ada6f10651535c669a96099e8c
DanielRasho <ray22933@uvg.edu.gt>
2024-07-27 19:20:20 -0600

    test: github test

--- tests/simpleTest.test.js
+++ tests/simpleTest.test.js
@@ -1,6 +1,21 @@
 const axios = require('axios')
 const { BASE_URL } = require('./jest.setup')
 
+const handle_axios_error = function (err) {
+  if (err.response) {
+    const custom_error = new Error(
+      err.response.statusText || 'Internal server error'
+    )
+    custom_error.status = err.response.status || 500
+    custom_error.description = err.response.data
+      ? err.response.data.message
+      : null
+    throw custom_error
+  }
+  throw new Error(err)
+}
+axios.interceptors.response.use((r) => r, handle_axios_error)
+
 describe('Check API health', () => {
   it('Should return 200 code', async () => {
     console.log(BASE_URL)
