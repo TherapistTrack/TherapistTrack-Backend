459172ab083d3e4cabca3b33e5b0bf6d4d61771e
xtsebas <thiagohugo2014@gmail.com>
2024-07-29 00:19:19 -0600

    feat: fileRoutes

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -6,4 +6,10 @@ const upload = require('../utils/multerConfig')
 router.post('/upload', upload.single('pdf'), fileController.uploadFile)
 router.get('/pdf/:filename', fileController.getFile)
 
+router.post('/create', fileController.createFile)
+router.put('/update/:name', fileController.updateFile)
+router.delete('/delete/:name', fileController.deleteFile)
+router.get('/listFiles', fileController.listFiles)
+router.get('/file/:name', fileController.getFileById)
+
 module.exports = router
