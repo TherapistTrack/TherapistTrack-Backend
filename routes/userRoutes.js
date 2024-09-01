6a978cc7ed9111b76ca75d30d73ba41b1b4a0a95
Gerson Ramirez <ram22281@uvg.edu.gt>
2024-08-31 22:21:08 -0600

    refactor: add scopes to endpoints to limit the access

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/userController')
+const { requiredScopes } = require('../middlewares/auth0Middleware')
 
-router.post('/register', userController.registerUser)
-router.delete('/delete', userController.deleteUser)
-router.put('/update', userController.updateUser)
-router.get('/list', userController.listUser)
+router.post(
+  '/register',
+  requiredScopes(['create:users']),
+  userController.registerUser
+)
+router.delete(
+  '/delete',
+  requiredScopes(['delete:users']),
+  userController.deleteUser
+)
+router.put(
+  '/update',
+  requiredScopes(['update:users']),
+  userController.updateUser
+)
+router.get('/list', requiredScopes(['read:users']), userController.listUser)
 
 module.exports = router
