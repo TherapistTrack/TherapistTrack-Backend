d867c08e757a4328ab2398feb658fa462dc59ad9
Gerson Ramirez <ram22281@uvg.edu.gt>
2024-08-31 22:28:59 -0600

    refactor: add request to get access token for testing suites

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -1,5 +1,35 @@
 require('dotenv').config()
+const axios = require('axios')
 
 const BASE_URL = `http://127.0.0.1:${process.env.API_PORT}`
 
-module.exports = { BASE_URL }
+let accessToken = null
+
+async function getAuthToken() {
+  try {
+    const response = await axios.post(
+      `${process.env.ISSUER_BASE_URL}oauth/token`,
+      {
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        audience: process.env.AUDIENCE,
+        grant_type: 'client_credentials'
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+    console.log(response.data)
+    return response.data.access_token
+  } catch (error) {
+    console.error('Error obtaining token:', error.message)
+    throw error
+  }
+}
+
+module.exports = {
+  BASE_URL,
+  getAuthToken
+}
