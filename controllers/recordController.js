07116b83a1a3999477e40c2baf805a11db613107
xtsebas <thiagohugo2014@gmail.com>
2024-09-05 16:27:51 -0600

    fix: record test

--- controllers/recordController.js
+++ controllers/recordController.js
@@ -5,8 +5,8 @@ exports.createRecord = async (req, res) => {
   const { doctor, template, patient } = req.body
 
   /*
-    const isValidObjectId = mongoose.Types.ObjectId.isValid(doctorId)
-    const isValidtemplateId = mongoose.Types.ObjectId.isValid(templateId)
+    const isValidObjectId = mongoose.Types.ObjectId.isValid(doctor)
+    const isValidtemplateId = mongoose.Types.ObjectId.isValid(template)
     if (!isValidObjectId || !isValidtemplateId) {
        return res.status(400).send({ status: 'error', message: 'Invalid record or template ID' });
     }
@@ -44,15 +44,15 @@ exports.editRecord = async (req, res) => {
   try {
     const record = await Record.findById(recordId)
 
+    if (record.doctor.toString() !== doctorId) {
+      return res.status(403).json({ error: 'Unauthorized' })
+    }
+
     const updatedRecord = await Record.findByIdAndUpdate(
       recordId,
       { patient },
       { new: true }
     )
-
-    if (record.doctorId !== doctorId) {
-      return res.status(403).json({ error: 'Unauthorized' })
-    }
 
     if (!updatedRecord) {
       return res.status(404).json({ error: 'Record not found' })
@@ -77,13 +77,17 @@ exports.deleteRecord = async (req, res) => {
 
   try {
     const record = await Record.findById(recordId)
+
     if (!record) {
       return res.status(404).json({ error: 'Record not found' })
     }
-    if (record.doctorId !== doctorId) {
+
+    if (record.doctor.toString() !== doctorId) {
       return res.status(403).json({ error: 'Unauthorized' })
     }
-    await record.remove()
+
+    await Record.findByIdAndDelete(recordId)
+
     res.status(200).json({ message: 'Record deleted successfully' })
   } catch (error) {
     res.status(500).json({ error: 'Error deleting the record' })
@@ -125,7 +129,7 @@ exports.getRecordById = async (req, res) => {
     if (!record) {
       return res.status(404).json({ error: 'Record not found' })
     }
-    if (record.doctorId !== doctorId) {
+    if (record.doctor.toString() !== doctorId) {
       return res.status(403).json({ error: 'Unauthorized' })
     }
 
--- tests/routes/recordRoutes.test.js
+++ tests/routes/recordRoutes.test.js
@@ -4,32 +4,117 @@ const { BASE_URL } = require('../jest.setup')
 describe('Record Controller Tests', () => {
   const baseUrl = BASE_URL + '/records'
   let testrecordID
-  const recordData = {
-    doctor: '66b453a2601a8e9fb46d8884',
-    template: '66b453a2601a8e9fb46d8885',
-    patient: {
-      names: 'Joe',
-      lastNames: 'Doe',
-      fields: [
-        {
-          name: 'Civil Status',
-          type: 'CHOICE',
-          options: ['single', 'married', 'separate'],
-          value: 'single',
-          required: true
-        }
-      ]
-    }
-  }
 
   it('should create a new record', async () => {
+    const recordData = {
+      doctor: '66b453a2601a8e9fb46d8884',
+      template: '66b453a2601a8e9fb46d8885',
+      patient: {
+        names: 'Joe',
+        lastNames: 'Doe',
+        fields: [
+          {
+            name: 'Civil Status',
+            type: 'CHOICE',
+            options: ['single', 'married', 'separate'],
+            value: 'single',
+            required: true
+          }
+        ]
+      }
+    }
+
     try {
       const response = await axios.post(`${baseUrl}/`, recordData)
       expect(response.status).toBe(201)
-      /*
-      expect(response.data.status).toBe('success')
       expect(response.data.message).toBe('Record created successfully')
-      testrecordID = response.data.recordId */
+      testrecordID = response.data.recordId
+    } catch (error) {
+      throw new Error(
+        `Test failed:\nStatus: ${error.response.status}\nData: ${JSON.stringify(error.response.data, null, 2)}`
+      )
+    }
+  })
+
+  it('should edit a record', async () => {
+    const recordData = {
+      doctorId: '66b453a2601a8e9fb46d8884',
+      recordId: testrecordID,
+      patient: {
+        names: 'Joe',
+        lastNames: 'Doe',
+        fields: [
+          {
+            name: 'Civil Status',
+            type: 'CHOICE',
+            options: ['single', 'married', 'separate'],
+            value: 'married',
+            required: true
+          },
+          {
+            name: 'Age',
+            type: 'NUMBER',
+            value: '30',
+            required: true
+          }
+        ]
+      }
+    }
+
+    try {
+      const response = await axios.put(`${baseUrl}/`, recordData)
+      expect(response.status).toBe(200)
+      expect(response.data.message).toBe('Record updated successfully')
+    } catch (error) {
+      throw new Error(
+        `Test failed:\nStatus: ${error.response.status}\nData: ${JSON.stringify(error.response.data, null, 2)}`
+      )
+    }
+  })
+
+  it('should get a record with a valid Id', async () => {
+    const recordData = {
+      recordId: testrecordID,
+      doctorId: '66b453a2601a8e9fb46d8884'
+    }
+    try {
+      const response = await axios.post(`${baseUrl}/records`, recordData)
+      expect(response.status).toBe(200)
+    } catch (error) {
+      throw new Error(
+        `Test failed:\nStatus: ${error.response.status}\nData: ${JSON.stringify(error.response.data, null, 2)}`
+      )
+    }
+  })
+
+  it('should list records of a doctor', async () => {
+    const recordData = {
+      doctorId: '66b453a2601a8e9fb46d8884',
+      limit: 5,
+      offset: 0
+    }
+    try {
+      const response = await axios.get(`${baseUrl}/list`, recordData)
+      console.log(response.data)
+      expect(response.status).toBe(200)
+      expect(response.data.records.length).toBeGreaterThan(0)
+      expect(response.data.total).toBeGreaterThan(0)
+    } catch (error) {
+      throw new Error(
+        `Test failed:\nStatus: ${error.response.status}\nData: ${JSON.stringify(error.response.data, null, 2)}`
+      )
+    }
+  })
+
+  it('should delete a record', async () => {
+    const recordData = {
+      doctorId: '66b453a2601a8e9fb46d8884',
+      recordId: testrecordID
+    }
+    try {
+      const response = await axios.delete(`${baseUrl}/`, { data: recordData })
+      expect(response.status).toBe(200)
+      expect(response.data.message).toBe('Record deleted successfully')
     } catch (error) {
       throw new Error(
         `Test failed:\nStatus: ${error.response.status}\nData: ${JSON.stringify(error.response.data, null, 2)}`
