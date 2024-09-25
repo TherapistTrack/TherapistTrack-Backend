719430abbc0d2827362a247e07758b1c246b8cf7
DanielRasho <ray22933@uvg.edu.gt>
2024-09-25 11:44:41 -0600

    feat: add more request COMMON MSG to dictionary

--- tests/errorMsg.js
+++ tests/errorMsg.js
@@ -3,10 +3,15 @@ const COMMON_MSG = {
   REQUEST_SUCCESS: 'Request succesful.',
   // 400
   MISSING_FIELDS: 'Missing Fields.',
+  // 403
   DOCTOR_IS_NOT_OWNER: 'Doctor is not the owner of template.',
   // 404
   DOCTOR_NOT_FOUND: 'Doctor not found.',
-  TEMPLATE_NOT_FOUND: 'Template not found.'
+  TEMPLATE_NOT_FOUND: 'Template not found.',
+  // 406
+  RECORDS_USING: 'Item with that id/name already exist',
+  // 409
+  OPERATION_REJECTED: 'Could not modify item since other resources depend on it'
 }
 
 module.exports = COMMON_MSG
