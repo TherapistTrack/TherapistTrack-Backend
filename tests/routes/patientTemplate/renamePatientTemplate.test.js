29ee8242b0feb6179185f0691fa78d68590be25d
DanielRasho <ray22933@uvg.edu.gt>
2024-09-25 11:45:09 -0600

    feat: create renamePatientTemplate tests

--- tests/routes/patientTemplate/renamePatientTemplate.test.js
+++ tests/routes/patientTemplate/renamePatientTemplate.test.js
@@ -1,33 +1,138 @@
+const axios = require('axios')
+const { BASE_URL, getAuthToken } = require('../../jest.setup')
+const COMMON_MSG = require('../../errorMsg')
+const {
+  createTestDoctor,
+  deleteUser,
+  checkFailRequest
+} = require('../../testHelpers')
+
 describe('Rename Patiente Template Tests', () => {
-  beforeAll(() => {
-    // TODO
-    // Create a 2 doctors
+  let doctor, secondDoctor, templateId
+
+  const REQUEST_URL = `${BASE_URL}/doctor/PatientTemplate`
+
+  const HEADERS = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${getAuthToken()}`,
+    Origin: 'http://localhost'
+  }
+
+  async function checkFailRenameRequest(body, expectedCode, expectedMsg) {
+    checkFailRequest(
+      'patch',
+      REQUEST_URL,
+      HEADERS,
+      {},
+      body,
+      expectedCode,
+      expectedMsg
+    )
+  }
+  beforeAll(async () => {
+    doctor = await createTestDoctor()
+    secondDoctor = await createTestDoctor()
+    templateId = await createTestPatientTemplate(
+      doctor.roleDependentInfo.id,
+      `testTemplate_${Date.now()}`,
+      [
+        {
+          name: 'Edad',
+          type: 'NUMBER',
+          required: true,
+          description: 'Edad del paciente'
+        }
+      ]
+    )
   })
 
-  afterAll(() => {
-    // TODO
-    // Remove patient
+  afterAll(async () => {
+    deleteUser(doctor.id)
+    deleteUser(secondDoctor.id)
   })
 
-  test('should rename with 200 a patient template correctly', async () => {
-    // Create template
-  })
   test("should fail with 400 to rename template if 'doctorId' is not provided", async () => {
-    // Create template
+    checkFailRenameRequest(
+      {
+        templateId: templateId,
+        name: 'NewName'
+      },
+      400,
+      COMMON_MSG.MISSING_FIELDS
+    )
   })
   test("should fail with 400 to rename template if 'templateId' is not provided", async () => {
-    // Create template
+    checkFailRenameRequest(
+      {
+        doctorId: doctor.roleDependentInfo.id,
+        name: 'NewName'
+      },
+      400,
+      COMMON_MSG.MISSING_FIELDS
+    )
   })
   test("should fail with 400 to rename template if 'name' is not provide", async () => {
-    // Create template
+    checkFailRenameRequest(
+      {
+        doctorId: doctor.roleDependentInfo.id,
+        templateId: templateId
+      },
+      400,
+      COMMON_MSG.MISSING_FIELDS
+    )
   })
   test("should fail with 403 to rename template if 'doctorid' exist but is not the owner of this template", async () => {
-    // Create template
+    checkFailRenameRequest(
+      {
+        doctorId: doctor.roleDependentInfo.id,
+        templateId: templateId,
+        name: 'NewName'
+      },
+      400,
+      COMMON_MSG.DOCTOR_IS_NOT_OWNER
+    )
   })
   test("should fail with 404 to rename template if 'doctorid' is not from a valid/active user", async () => {
-    // Create template
+    checkFailRenameRequest(
+      {
+        doctorId: 'nonExistentDoctor',
+        templateId: templateId,
+        name: 'NewName'
+      },
+      400,
+      COMMON_MSG.DOCTOR_NOT_FOUND
+    )
   })
   test("should fail with 404 to rename template if 'template' is not from a valid/active template", async () => {
-    // Create template
+    checkFailRenameRequest(
+      {
+        doctorId: doctor.roleDependentInfo.id,
+        templateId: 'notExistentTemplate',
+        name: 'NewName'
+      },
+      400,
+      COMMON_MSG.TEMPLATE_NOT_FOUND
+    )
+  })
+  test('should rename with 200 a patient template correctly', async () => {
+    try {
+      const response = await axios.delete(
+        REQUEST_URL,
+        {
+          doctorId: doctor.roleDependentInfo.id,
+          templateID: templateId,
+          name: 'newName'
+        },
+        { headers: HEADERS }
+      )
+      expect(response.status).toBe(200)
+      expect(response.data.message).toBe(COMMON_MSG.REQUEST_SUCCESS)
+    } catch (error) {
+      console.error(
+        'Error renaming template:',
+        error.response ? error.response.data : error.message
+      )
+      throw error
+    }
   })
 })
