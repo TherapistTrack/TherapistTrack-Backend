831f9657f1c619b75914937356535c1fc4926b4f
EdwinOrtegaK <andyortegakou@.com.mx>
2024-09-25 14:29:56 -0600

    test: created and simplified tests based on the use of checkFailRequest

--- tests/routes/patientTemplate/listPatientTemplate.test.js
+++ tests/routes/patientTemplate/listPatientTemplate.test.js
@@ -1,22 +1,138 @@
+const axios = require('axios')
+const { BASE_URL, getAuthToken } = require('../../jest.setup')
+const COMMON_MSG = require('../../../utils/errorMsg')
+const {
+  createTestDoctor,
+  deleteUser,
+  checkFailRequest
+} = require('../../testHelpers')
+
 describe('List Patiente Templates Tests', () => {
-  beforeAll(() => {
-    // TODO
-    // Create 3 templates
+  let doctor
+
+  const REQUEST_URL = `${BASE_URL}/doctor/PatientTemplate/list`
+
+  const HEADERS = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${getAuthToken()}`,
+    Origin: 'http://localhost'
+  }
+
+  async function checkFailListRequest(queryParams, expectedCode, expectedMsg) {
+    await checkFailRequest(
+      'get',
+      REQUEST_URL,
+      HEADERS,
+      queryParams,
+      {},
+      expectedCode,
+      expectedMsg
+    )
+  }
+
+  beforeAll(async () => {
+    doctor = await createTestDoctor()
+
+    // Crear varias plantillas de paciente para usarlas en los tests
+    templateId1 = await createTestPatientTemplate(
+      doctor.roleDependentInfo.id,
+      `testTemplate1_${Date.now()}`,
+      [
+        {
+          name: 'Edad',
+          type: 'NUMBER',
+          required: true,
+          description: 'Edad del paciente'
+        },
+        {
+          name: 'Estado Civil',
+          type: 'CHOICE',
+          options: ['Soltero', 'Casado'],
+          required: true,
+          description: 'Estado civil del paciente'
+        }
+      ]
+    )
+
+    templateId2 = await createTestPatientTemplate(
+      doctor.roleDependentInfo.id,
+      `testTemplate2_${Date.now()}`,
+      [
+        {
+          name: 'Altura',
+          type: 'NUMBER',
+          required: true,
+          description: 'Altura del paciente'
+        },
+        {
+          name: 'Peso',
+          type: 'NUMBER',
+          required: true,
+          description: 'Peso del paciente'
+        }
+      ]
+    )
+
+    templateId3 = await createTestPatientTemplate(
+      doctor.roleDependentInfo.id,
+      `testTemplate3_${Date.now()}`,
+      [
+        {
+          name: 'Tipo de Sangre',
+          type: 'CHOICE',
+          options: ['A+', 'B+', 'O+', 'AB+'],
+          required: true,
+          description: 'Tipo de sangre del paciente'
+        },
+        {
+          name: 'Alergias',
+          type: 'TEXT',
+          required: false,
+          description: 'Alergias conocidas del paciente'
+        }
+      ]
+    )
   })
 
-  afterAll(() => {
-    // TODO
-    // Remove patient
+  afterAll(async () => {
+    await deleteUser(doctorId)
   })
 
+  // Test para obtener la lista de plantillas correctamente
   test('should fetch with 200 a list of 3 templates', async () => {
-    // Create template
-    // Check that the result fetch all the necessary fields.
+    try {
+      const response = await axios.get(
+        REQUEST_URL,
+        {
+          doctorId: doctor.roleDependentInfo.id
+        },
+        { headers: HEADERS }
+      )
+      expect(response.status).toBe(200)
+      expect(response.data).toHaveProperty('templates')
+      expect(response.data.templates.length).toBe(3)
+      expect(response.data.message).toBe(COMMON_MSG.REQUEST_SUCCESS)
+    } catch (error) {
+      console.error(
+        'Error fetching patient template list:',
+        error.response ? error.response.data : error.message
+      )
+      throw error
+    }
   })
+
   test("should fail with 400 to list template if 'doctorId' is not provided", async () => {
-    // Create template
+    await checkFailListRequest({}, 400, COMMON_MSG.MISSING_FIELDS)
   })
+
   test("should fail with 404 to list template if 'doctorid' is not from a active/valid user", async () => {
-    // Create template
+    const invalidDoctorId = 'invalidDoctorId123'
+    await checkFailListRequest(
+      {
+        doctorId: invalidDoctorId
+      },
+      404,
+      COMMON_MSG.DOCTOR_NOT_FOUND
+    )
   })
 })
