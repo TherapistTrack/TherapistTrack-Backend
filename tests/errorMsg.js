9a483d68cd400d392a8041852551651972a63542
DanielRasho <ray22933@uvg.edu.gt>
2024-09-25 10:37:17 -0600

    feat: create common api responses dictionary

---
+++ tests/errorMsg.js
@@ -0,0 +1,12 @@
+const COMMON_MSG = {
+  // 200
+  REQUEST_SUCCESS: 'Request succesful.',
+  // 400
+  MISSING_FIELDS: 'Missing Fields.',
+  DOCTOR_IS_NOT_OWNER: 'Doctor is not the owner of template.',
+  // 404
+  DOCTOR_NOT_FOUND: 'Doctor not found.',
+  TEMPLATE_NOT_FOUND: 'Template not found.'
+}
+
+module.exports = COMMON_MSG
