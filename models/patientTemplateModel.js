812ba45fa5c324c6d2bab589ea66f069a32dbf81
xtsebas <thiagohugo2014@gmail.com>
2024-09-24 19:31:07 -0600

    fix: description added

--- models/patientTemplateModel.js
+++ models/patientTemplateModel.js
@@ -4,6 +4,7 @@ const fieldSchema = new mongoose.Schema({
   name: { type: String, required: true },
   type: { type: String, required: true },
   options: [String],
+  description: { type: String, required: true },
   required: { type: Boolean, default: true }
 })
 
