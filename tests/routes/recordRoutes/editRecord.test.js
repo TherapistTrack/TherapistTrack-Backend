95bb094d3359fe8269e30ef0508a8aa41bc89d5e
DanielRasho <ray22933@uvg.edu.gt>
2024-10-13 10:51:17 -0600

    feat: define edit records tests

--- tests/routes/recordRoutes/editRecord.test.js
+++ tests/routes/recordRoutes/editRecord.test.js
@@ -2,88 +2,113 @@ const axios = require('axios')
 const { BASE_URL } = require('../../jest.setup')
 
 describe('PUT /records/ endpoint', () => {
-  it('should edit a record', async () => {
-    const recordData = {
-      doctorId: '66b453a2601a8e9fb46d8884',
-      recordId: testrecordID,
-      patient: {
-        names: 'Joe',
-        lastNames: 'Doe',
-        fields: [
-          {
-            name: 'Civil Status',
-            type: 'CHOICE',
-            options: ['single', 'married', 'separate'],
-            value: 'married',
-            required: true
-          },
-          {
-            name: 'Age',
-            type: 'NUMBER',
-            value: '30',
-            required: true
-          }
-        ]
-      }
-    }
+  // TODO:
+  test('should fail with 400 if recordId is not passed', async () => {})
 
-    try {
-      const response = await axios.put(`${baseUrl}/`, recordData, { headers })
-      expect(response.status).toBe(200)
-      expect(response.data.message).toBe('Record updated successfully')
-    } catch (error) {
-      throw new Error(
-        `Test failed:\nStatus: ${error.response.status}\nData: ${JSON.stringify(error.response.data, null, 2)}`
-      )
-    }
+  // TODO:
+  test('should fail with 400 if doctorId is not passed', async () => {})
+
+  // TODO:
+  test('should fail with 400 if patient is not passed', async () => {})
+
+  // TODO:
+  test('should fail with 400 if patient is passed malformed (missing fields)', async () => {})
+
+  // TODO:
+  test('should fail with 400 if patient does not include all the fields', async () => {})
+
+  // TODO:
+  test('should fail with 404 if doctorId is from a non-existent/active user', async () => {})
+
+  // TODO:
+  test('should fail with 404 if recordId is from a non-existent record', async () => {})
+
+  // TODO:
+  test('should fail with 404 if not all fields defined by the template are not sent', async () => {
+    // create a new message for that in COMMON_MSG
   })
 
-  /*Este test sirve una vez tengamos la coleccion de dcotros y templates
-  it('should return 400 when trying to edit a record with invalid record ID', async () => {
-    const invalidEditData = {
-      doctorId: '66b453a2601a8e9fb46d8884', // ID válido de doctor
-      recordId: 'invalidRecordId',
-      patient: 'Updated Patient Info'
-    }
-  
-    try {
-      await axios.put(`${baseUrl}/`, invalidEditData, { headers })
-    } catch (error) {
-      expect(error.response.status).toBe(400)
-      expect(error.response.data.message).toBe('Invalid record ID')
-    }
-  })*/
+  // ==================
+  // === TEXT ===
+  // ==================
+  // TODO:
+  test('should fail with 405 when passing NUMBER value for TEXT field', async () => {})
 
-  it('should return 403 when doctor is unauthorized to edit a record', async () => {
-    const unauthorizedEditData = {
-      doctorId: 'anotherDoctorId', // Un doctor diferente al del registro
-      recordId: testrecordID,
-      patient: {
-        names: 'Joe',
-        lastNames: 'Doe',
-        fields: [
-          {
-            name: 'Civil Status',
-            type: 'CHOICE',
-            options: ['single', 'married', 'separate'],
-            value: 'married',
-            required: true
-          },
-          {
-            name: 'Age',
-            type: 'NUMBER',
-            value: '30',
-            required: true
-          }
-        ]
-      }
-    }
+  // TODO:
+  test('should fail with 405 when passing BOOLEAN value for TEXT field', async () => {})
 
-    try {
-      await axios.put(`${baseUrl}/`, unauthorizedEditData, { headers })
-    } catch (error) {
-      expect(error.response.status).toBe(403)
-      expect(error.response.data.error).toBe('Unauthorized')
-    }
+  // TODO:
+  test('should fail with 405 when passing ARRAY value for TEXT field', async () => {})
+
+  // ==================
+  // === SHORT_TEXT ===
+  // ==================
+  // TODO:
+  test('should fail with 405 when passing NUMBER value for SHORT_TEXT field', async () => {})
+
+  // TODO:
+  test('should fail with 405 when passing BOOLEAN value for SHORT_TEXT field', async () => {})
+
+  // TODO:
+  test('should fail with 405 when passing ARRAY value for SHORT_TEXT field', async () => {})
+
+  // ==================
+  // === SHORT_TEXT ===
+  // ==================
+  // TODO:
+  test('should fail with 405 when passing TEXT value for NUMBER field', async () => {})
+  // TODO:
+  test('should fail with 405 when passing BOOLEAN value for NUMBER field', async () => {})
+
+  // TODO:
+  test('should fail with 405 when passing ARRAY value for NUMBER field', async () => {})
+
+  // ==================
+  // === NUMBER ===
+  // ==================
+  // TODO:
+  test('should fail with 405 when passing FLOAT value for NUMBER field', async () => {
+    // Number field just accept integers
   })
+  // TODO:
+  test('should fail with 405 when passing TEXT value for NUMBER field', async () => {})
+  // TODO:
+  test('should fail with 405 when passing BOOLEAN value for NUMBER field', async () => {})
+  // TODO:
+  test('should fail with 405 when passing ARRAY value for FLOAT field', async () => {})
+
+  // ==================
+  // === FLOAT ===
+  // ==================
+  // TODO:
+  test('should fail with 405 when passing TEXT value for FLOAT field', async () => {})
+  // TODO:
+  test('should fail with 405 when passing BOOLEAN value for FLOAT field', async () => {})
+  // TODO:
+  test('should fail with 405 when passing ARRAY value for FLOAT field', async () => {})
+
+  // ==================
+  // === CHOICE =======
+  // ==================
+  // TODO:
+  test('should fail with 405 when passing NUMBER values to CHOICE', async () => {})
+
+  // TODO:
+  test('should fail with 405 when passing BOOLEAN values to CHOICE', async () => {})
+  // TODO:
+  test('should fail with 405 when passing VALUE that is not within CHOICE value', async () => {})
+  // TODO:
+  test('should fail with 405 when passing ARRAY value for CHOICE field', async () => {})
+
+  // ==================
+  // === DATE =======
+  // ==================
+  // TODO:
+  test('should fail with 405 when passing TEXT value for DATE field', async () => {})
+  // TODO:
+  test('should fail with 405 when passing BOOLEAN value for DATE field', async () => {})
+  // TODO:
+  test('should fail with 405 when passing NUMBER value for DATE field', async () => {})
+  // TODO:
+  test('should fail with 405 when passing ARRAY value for DATE field', async () => {})
 })
