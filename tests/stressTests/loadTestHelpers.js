fdbea89ffd1ce83f079faf59fc11b6a2e671d230
Gerson Ramirez <ram22281@uvg.edu.gt>
2024-11-18 17:26:28 -0600

    feat: add new functions to generate data for testing

--- tests/stressTests/loadTestHelpers.js
+++ tests/stressTests/loadTestHelpers.js
@@ -13,6 +13,74 @@ function generateObjectId() {
   return timestamp + randomPart
 }
 
+/**
+ * Generates user data based on the provided role.
+ *
+ * @param {string} role - The role of the user, either 'Doctor' or another role.
+ * @returns {Object} User data - payload for creating a user.
+ */
+function generateUserData(role) {
+  const randomId = generateObjectId()
+
+  return {
+    id: randomId,
+    names: `TestUser`,
+    lastNames: 'User',
+    phones: ['12345678'],
+    rol: role,
+    mails: [`test-${randomId}@example.com`],
+    roleDependentInfo:
+      role === 'Doctor'
+        ? {
+            collegiateNumber: '12345',
+            specialty: 'testSpecialty'
+          }
+        : {
+            startDate: '08/14/2024',
+            endDate: '08/15/2024',
+            DPI: '2340934'
+          }
+  }
+}
+
+/**
+ * Generates patient template data.
+ *
+ * @returns {Object} Patient template data - payload for creating a patient template.
+ */
+function generatePatientTemplateData(ownerId) {
+  const timestamp = new Date().toISOString()
+
+  return {
+    doctorId: ownerId,
+    name: `Test Template - ${timestamp}`,
+    categories: ['Test Category'],
+    fields: [
+      {
+        name: 'Test Field',
+        type: 'TEXT',
+        options: [],
+        required: true,
+        description: 'Test Description'
+      }
+    ]
+  }
+}
+
+/**
+ * Generates a random string of the specified length.
+ *
+ * @param {number} length - The length of the random string.
+ * @returns {string} Random string.
+ */
+function generateRandomString(length) {
+  return Math.random()
+    .toString(36)
+    .substring(2, length + 2)
+}
+
 module.exports = {
-  generateObjectId
+  generateUserData,
+  generatePatientTemplateData,
+  generateRandomString
 }
